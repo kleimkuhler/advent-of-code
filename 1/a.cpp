a6e992bc0e3af6cc4b19027e93b1e5ec485efb5b
Kevin Leimkuhler <me@kleimkuhler.com>
2017-12-01 12:11:45 -0800

    Add 1a solution

---
+++ 1/a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;   using std::cin;
+using std::endl;   using std::string;
+
+int main()
+{
+  string s;
+  cin >> s;
+
+  int sum = 0;
+  string::size_type j;
+  for (string::size_type i = 0; i != s.size(); ++i) {
+    // j = cyclical i+1 index
+    j = (i + 1) % s.size();
+
+    int i_val = s[i] - '0';
+    int j_val = s[j] - '0';
+    if (i_val == j_val)
+      sum += i_val;
+  }
+  cout << sum << endl;
+  return 0;
+}
