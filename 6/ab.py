508f95d2093ab19a6bc35f2776362b62b9b895c7
Kevin Leimkuhler <kevleimkuhler@gmail.com>
2017-12-06 08:58:55 -0800

    Cleanup day 6 solutions

--- 6/ab.py
+++ 6/ab.py
@@ -2,41 +2,24 @@ from itertools import count
 from itertools import cycle
 from itertools import islice
 
-def a(A):
-    configs = set()
-    for steps in count(1):
-        bank = A.index(max(A))
-        dist, A[bank] = A[bank], 0
-        banks = cycle(range(len(A)))
-        banks_start = islice(banks, bank+1, None)
-
-        while dist != 0:
-            A[next(banks_start)] += 1
-            dist -= 1
-
-        if tuple(A) not in configs:
-            configs.add(tuple(A))
-        else:
-            return steps
-
-def b(A):
+def ab(A):
     configs = dict()
     for steps in count(1):
         bank = A.index(max(A))
         dist, A[bank] = A[bank], 0
-        banks = cycle(range(len(A)))
-        banks_start = islice(banks, bank+1, None)
+        banks = islice(cycle(range(len(A))), bank+1, None)
 
         while dist != 0:
-            A[next(banks_start)] += 1
+            A[next(banks)] += 1
             dist -= 1
 
         if tuple(A) not in configs:
             configs[tuple(A)] = steps
         else:
-            return steps - configs[tuple(A)]
+            return (steps, steps - configs[tuple(A)])
 
 with open("./input.txt") as f:
     memory_banks = [int(x.strip()) for x in f.readline().split()]
-    print("a: {}".format(a(memory_banks[:])))
-    print("b: {}".format(b(memory_banks[:])))
+    a, b = [x for x in ab(memory_banks[:])]
+    print("a: {}".format(a))
+    print("b: {}".format(b))
