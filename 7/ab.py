176b5d20b96ae43b38f5fb6daaaec38cbc565a39
Kevin Leimkuhler <kevleimkuhler@gmail.com>
2017-12-08 13:16:04 -0800

    Improving part b to not return whole branch

--- 7/ab.py
+++ 7/ab.py
@@ -1,54 +1,99 @@
 from collections import Counter
 from functools import reduce
 
-def find_root(adj_list):
+def find_roots(dag):
+    # all nodes not visited are roots
     visited = []
-    for node, props in adj_list.items():
-        visited.extend(props[1])
-    root = set(adj_list.keys()) ^ set(visited)
-    return root.pop()
+    for node, props in dag.items():
+        visited.extend(props['children'])
+    roots = set(dag.keys()) ^ set(visited)
+    return roots
 
-def find_unbalanced(weights, delta):
-    counter = Counter(weights)
-    unique = counter.most_common()[-1:]
-    return unique[0][0]
-
-def find_weight(adj_list, root):
-    if not adj_list[root][1]:
-        return adj_list[root][0]
-
-    child_weights = []
-    for child in adj_list[root][1]:
-        child_weights.append(find_weight(adj_list, child))
-
-    child_weight = set(child_weights)
-    if len(child_weight) != 1:
-        delta = child_weight.pop() - child_weight.pop()
-        unbalanced_node = find_unbalanced(child_weights, delta)
-        unbalanced_root = child_weights.index(unbalanced_node)
-        print("{}".format(adj_list[adj_list[root][1][unbalanced_root]][0]-delta))
-        return adj_list[root][0] + reduce(lambda x, y : x + y, child_weights)
-    else:
-        child_weight = reduce(lambda x, y : x + y, child_weights)
-        root_weight = adj_list[root][0] + child_weight
-        return root_weight
-
-with open("./input.txt") as f:
-    adj_list = {}
-    nodes = [line.strip().split() for line in f]
-
+def make_dag(nodes):
+    dag = {}
     for node in nodes:
-        props = []
-        props.append(int(node[1].strip('()')))
-        neighbors = []
+        props = {}
+        props['weight'] = int(node[1].strip('()'))
+        children = []
         try:
             for i in range(3, len(node)+1):
-                neighbors.append(node[i].strip(','))
+                children.append(node[i].strip(','))
         except IndexError:
             pass
-        props.append(neighbors)
-        adj_list[node[0]] = props
+        props['children'] = children
+        dag[node[0]] = props
 
-    root = find_root(adj_list)
-    print("a: {}".format(root))
-    print("b: {}".format(find_weight(adj_list, root)))
+    return dag
+
+def reverse_dag(dag):
+    # populate reverse dag with nodes & weights
+    rev_dag = {}
+    for node, props in dag.items():
+        rev_props = {}
+        rev_props['weight'] = props['weight']
+        rev_props['children'] = []
+        rev_dag[node] = rev_props
+
+    # reverse children
+    for node, props in dag.items():
+        for child in props['children']:
+            rev_dag[child]['children'].append(node)
+
+    return rev_dag
+
+def find_weight(dag, root, total_weights):
+    if not dag[root]['children']:
+        return dag[root]['weight']
+
+    if root in total_weights:
+        return total_weights[root]
+
+    child_weights = []
+    for child in dag[root]['children']:
+        child_weights.append(find_weight(dag, child, total_weights))
+
+    child_weight = reduce(lambda x, y : x + y, child_weights)
+    return dag[root]['weight'] + child_weight
+
+def find_bad_weight(root_weights):
+    weights = dict(Counter(root_weights).most_common())
+    return list(weights.keys()).pop()
+
+def a(dag):
+    roots = find_roots(dag)
+    return roots.pop()
+
+def b(dag):
+    total_weights = {}
+    rev_dag = reverse_dag(dag)
+
+    while rev_dag:
+        # strip roots from rev_dag
+        strip_roots = find_roots(rev_dag)
+        for root in strip_roots:
+            del rev_dag[root]
+
+        # roots of rev_dag are now an inner layer from dag leaves
+        # iterate through these to find unbalanced nodes
+        roots = list(find_roots(rev_dag))
+        for root in roots:
+            children_weights = []
+            for child in dag[root]['children']:
+                total_weights[child] = find_weight(dag, child, total_weights)
+                children_weights.append(total_weights[child])
+
+            unique_weights = set(children_weights)
+            if len(unique_weights) != 1:
+                # find bad_weight and lookup unbalanced root weight
+                delta = unique_weights.pop() - unique_weights.pop()
+                bad_weight = find_bad_weight(children_weights)
+                bad_node = list(total_weights.keys())[list(total_weights.values()).index(bad_weight)]
+                return dag[bad_node]['weight'] - delta
+
+with open("./input.txt") as f:
+    dag = {}
+    nodes = [line.strip().split() for line in f]
+    dag = make_dag(nodes)
+    rev_dag = reverse_dag(dag)
+    print("a: {}".format(a(dag)))
+    print("b: {}".format(b(dag)))
