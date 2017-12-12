5432b1f147b75ab3e2a053d1097b98dfb1474fbb
Kevin Leimkuhler <me@kleimkuhler.com>
2017-12-11 18:03:35 -0800

    Make part 1 solution idiomatic

--- 10/puzzle.py
+++ 10/puzzle.py
@@ -1,16 +1,12 @@
-def reverse(hash, x, length, inc):
-    for i in range(0, (length//2)):
-        lo = inc(x, i)
-        hi = inc(x-i-1, length)
-        hash[lo], hash[hi] = hash[hi], hash[lo]
-    return hash
-
 def compute(lengths):
     hash = [x for x in range(0, 256)]
     inc = lambda x, y: (x + y) % len(hash)
     pos, skip = 0, 0
     for length in lengths:
-        reverse(hash, pos, length, inc)
+        indices = [inc(pos, x) for x in range(length)]
+        values = reversed([hash[x] for x in indices])
+        for i, v in zip(indices, values):
+            hash[i] = v
         pos += inc(length, skip)
         skip += 1
     return hash
