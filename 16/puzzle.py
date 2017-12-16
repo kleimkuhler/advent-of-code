4dfcecc6d554edad136bc196f8013e96e7d7a731
Kevin Leimkuhler <me@kleimkuhler.com>
2017-12-16 15:43:21 -0800

    Make day 16 solution more pythonic
    I would not argue this solution is more efficient in time or space
    than the previous iteration, however I would argue it's more pythonic
    which I'm trying to focus on. I still think my dance generator could
    use some improvement.

--- 16/puzzle.py
+++ 16/puzzle.py
@@ -1,28 +1,32 @@
-cat = ''.join
-
-def dance(programs, moves, repeat=1):
-    seen = []
-    for i in range(repeat):
-        perm = cat(programs)
-        if perm in seen:
-            return seen[repeat % len(seen)]
-        seen.append(perm)
-
+def dance(programs, moves):
+    "Generate an infinite sequence of program permutations according to moves."
+    while True:
         for move in moves:
             if move[0] == 's':
                 shift = move[1:]
                 programs = programs[len(programs)-int(shift):] + programs[:len(programs)-int(shift)]
-            if move[0] == 'x':
+            elif move[0] == 'x':
                 a, b = move[1:].split('/')
                 programs[int(a)], programs[int(b)] = programs[int(b)], programs[int(a)]
-            if move[0] == 'p':
+            elif move[0] == 'p':
                 ai = programs.index(move[1])
                 bi = programs.index(move[3])
                 programs[ai], programs[bi] = programs[bi], programs[ai]
-    return programs
+        yield ''.join(programs)
+
+def routine(D, repeat):
+    "Which program permutation will the `repeat` repetition be?"
+    seen = []
+    for _ in range(repeat):
+        perm = next(D)
+        if perm in seen:
+            return seen[repeat % len(seen) - 1]
+        seen.append(perm)
 
 with open('./input.txt') as f:
     programs = [x for x in 'abcdefghijklmnop']
     moves = f.read().strip().split(',')
-    print('1: {}'.format(cat(dance(programs[:], moves))))
-    print('2: {}'.format(cat(dance(programs[:], moves, 1000000000))))
+    D = lambda programs, moves: dance(programs, moves)
+
+    print('1: {}'.format(next(D(programs[:], moves))))
+    print('2: {}'.format(routine(D(programs[:], moves), 10**9)))
