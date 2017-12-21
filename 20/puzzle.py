25eec1b94cc99fd178786675152b4b080a0e7088
Kevin Leimkuhler <me@kleimkuhler.com>
2017-12-21 13:15:09 -0500

    Add day 20 part 2 solution

--- 20/puzzle.py
+++ 20/puzzle.py
@@ -5,7 +5,7 @@ from math import sqrt
 
 def group(A, n):
     "Make m groups of size n."
-    return list(map(list, [A[x:x+n] for x in range(0, len(A), n)]))
+    return tuple(map(tuple, [A[x:x+n] for x in range(0, len(A), n)]))
 
 def magnitude(vec):
     "Return the magnitude of a 3 dimensional vector."
@@ -28,18 +28,46 @@ def closest(pos, vel, accel):
     minpos = minimum_reduce(pos, magnitude, minvel)
     return minpos[0]
 
-def generator(pos, vel, accel):
+def ticker(pos, vel, accel):
+    "Tick generator that removes any particles in the same position."
     while True:
-        pass
+        for p in set(pos):
+            vel[p] = tuple(map(sum, zip(vel[p], accel[p])))
+            pos[p] = tuple(map(sum, zip(pos[p], vel[p])))
+        duplicates = defaultdict(list)
+        for k, v in pos.items():
+            duplicates[v].append(k)
+        for v in duplicates.values():
+            if len(v) > 1:
+                for p in v:
+                    pos.pop(p)
+                    vel.pop(p)
+                    accel.pop(p)
+        yield (pos, vel, accel)
 
-def collision_reduce(pos, vel, accel):
-    while True:
-        pass
+def uncatchable(pos, vel, accel):
+    """Return true if the sorted list of keys for the position, velocity,
+    and acceleration magnitudes are equal, meaning they are in an
+    unchangeable state"""
+    magpos = {k: magnitude(v) for k, v in pos.items()}
+    magvel = {k: magnitude(v) for k, v in vel.items()}
+    magaccel = {k: magnitude(v) for k, v in accel.items()}
+    sortpos = [k for k in sorted(magpos, key=magpos.get)]
+    sortvel = [k for k in sorted(magvel, key=magvel.get)]
+    sortaccel = [k for k in sorted(magaccel, key=magaccel.get)]
+    return sortpos == sortvel
+            
+def collide(pos, vel, accel):
+    "Tick particle states until system is in an uncatchable state."
+    T = ticker(pos, vel, accel)
+    for _ in range(1000):
+        next(T)
+    return len(set(pos))
 
 with open('./input.txt') as f:
     pos, vel, accel = defaultdict(list), defaultdict(list), defaultdict(list)
     for i, line in enumerate(f):
-        p, v, a = group(list(map(int, re.findall(r'-?\d+', line))), 3)
+        p, v, a = group(tuple(map(int, re.findall(r'-?\d+', line))), 3)
         pos[i], vel[i], accel[i] = p, v, a
     print('1: {}'.format(closest(pos, vel, accel)))
     print('2: {}'.format(collide(pos, vel, accel)))
