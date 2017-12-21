002f8a0b9960f3c4413a950f9ea9bed8993b601a
Kevin Leimkuhler <me@kleimkuhler.com>
2017-12-20 23:15:09 -0500

    Add day 20 part 1 solution

---
+++ 20/puzzle.py
@@ -0,0 +1,45 @@
+import re
+
+from collections import defaultdict
+from math import sqrt
+
+def group(A, n):
+    "Make m groups of size n."
+    return list(map(list, [A[x:x+n] for x in range(0, len(A), n)]))
+
+def magnitude(vec):
+    "Return the magnitude of a 3 dimensional vector."
+    x, y, z = vec
+    return sqrt(abs(x) + abs(y) + abs(z))
+
+def minimum_reduce(D, comp, constraint):
+    """Reduce the {key: value} pairs of a dict down to the minimum `comp()`
+    values constrained by the keys in `constraint`"""
+    return [k for k, v in D.items()
+            if k in constraint
+            and comp(v) == min(map(comp, [D[x] for x in constraint]))]
+
+def closest(pos, vel, accel):
+    """Find the particle closest to (0, 0, 0) after an infinte amount of ticks
+    by reducing to the minimum set of accelerations, velocities, and positions
+    respectively"""
+    minaccel = minimum_reduce(accel, magnitude, set(accel))
+    minvel = minimum_reduce(vel, magnitude, minaccel)
+    minpos = minimum_reduce(pos, magnitude, minvel)
+    return minpos[0]
+
+def generator(pos, vel, accel):
+    while True:
+        pass
+
+def collision_reduce(pos, vel, accel):
+    while True:
+        pass
+
+with open('./input.txt') as f:
+    pos, vel, accel = defaultdict(list), defaultdict(list), defaultdict(list)
+    for i, line in enumerate(f):
+        p, v, a = group(list(map(int, re.findall(r'-?\d+', line))), 3)
+        pos[i], vel[i], accel[i] = p, v, a
+    print('1: {}'.format(closest(pos, vel, accel)))
+    print('2: {}'.format(collide(pos, vel, accel)))
